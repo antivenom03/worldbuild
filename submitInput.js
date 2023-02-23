e1377b736e48b2efb4aa25ce8e58f7d487575613
antivenom <jojoivy03@gmail.com>
2023-02-22 20:29:18 -0800

    Update submitInput.js

--- submitInput.js
+++ submitInput.js
@@ -2,36 +2,24 @@ function submitInput() {
   const userInput = document.getElementById("user-input").value;
   const currentPassage = State.passage;
 
-  const url = 'https://api.github.com/repos/Antivenom03/worldbuild/contents/input.html';
-  const headers = new Headers();
-  headers.append('Authorization', `Bearer ${{ secrets.ACCESS_TOKEN }}`);
+  const inputEvent = new CustomEvent("input", {
+    detail: {
+      user_input: userInput,
+      current_passage: currentPassage
+    },
+  });
 
-  // Step 1: Retrieve the current content of the file
-  fetch(url, {
-    headers: headers
-  })
-  .then(response => response.json())
-  .then(data => {
-    const currentContent = atob(data.content);
+  document.dispatchEvent(inputEvent);
 
-    // Step 2: Modify the current content with the new user input
-    const newContent = `${currentContent}\n${currentPassage}: ${userInput}`;
-
-    // Step 3: Write the modified content back to the file
-    const updatedData = {
-      message: `Add user input from ${currentPassage}`,
-      content: btoa(newContent),
-      sha: data.sha,
-      branch: 'main'
-    };
-
-    return fetch(url, {
-      method: 'PUT',
-      headers: headers,
-      body: JSON.stringify(updatedData)
-    });
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+  fetch('https://api.github.com/repos/Antivenom03/worldbuild/actions/workflows/main.yml/dispatches', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      ref: 'main'
+    })
+  });
 }
